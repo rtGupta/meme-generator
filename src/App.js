fb0602923d869053346cab5e230e97069af98791
Aarti Gupta <rtgupta@Aartis-MacBook-Air.local>
2024-01-20 16:30:28 -0700

    Header component complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
+import "./styles.css";
 
 export default function App() {
     return (
-        <h1>App Component!</h1>
+        <div>
+            <Header />
+        </div>
     )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header--logo" src="images/Troll Face.png" alt="meme icon" />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--sub-title">React Course - Project 3</h4>
+        </header>
+    )
+}
